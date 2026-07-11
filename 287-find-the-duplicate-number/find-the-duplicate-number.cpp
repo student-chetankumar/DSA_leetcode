79e3801475aa8efd2c7f42dd2e403f9b0c384129
student-chetankumar <chetankumar98672@gmail.com>
2026-07-11 13:13:40 +0530

    Time: 38 ms (22.98%) | Memory: 65.1 MB (41.66%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-//      sort(nums.begin(),nums.end());
-//    for(int i=0;i<nums.size()-1;i++)
-//    {
-//       if(nums[i]==nums[i+1])
-//       {
-//         return nums[i];
-//       }
+     sort(nums.begin(),nums.end());
+   for(int i=0;i<nums.size()-1;i++)
+   {
+      if(nums[i]==nums[i+1])
+      {
+        return nums[i];
+      }
   
-//    }
-//         return -1;
-    int s=0,e=nums.size()-1;
-    int ans=-1;
-    while(s<=e){
-        int mid=s+(e-s)/2;
+   }
+        return -1;
+    // int s=0,e=nums.size()-1;
+    // int ans=-1;
+    // while(s<=e){
+    //     int mid=s+(e-s)/2;
 
-        int count=0;
-        for(int num:nums){
-            if(num<=mid){
-                count++;
-            }
-        }
+    //     int count=0;
+    //     for(int num:nums){
+    //         if(num<=mid){
+    //             count++;
+    //         }
+    //     }
 
-        if(count>mid){
-            ans=mid;
-            e=mid-1;
-        }else{
-            s=mid+1;
-        }
-    }
-    return ans;
+    //     if(count>mid){
+    //         ans=mid;
+    //         e=mid-1;
+    //     }else{
+    //         s=mid+1;
+    //     }
+    // }
+    // return ans;
     }
 };
