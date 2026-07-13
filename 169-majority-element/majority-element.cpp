7e786353d74e5f28b08d6788b31f54fc08a5da71
student-chetankumar <chetankumar98672@gmail.com>
2026-07-13 18:57:46 +0530

    Time: 3 ms (27.93%) | Memory: 28.3 MB (34.09%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -17,28 +17,35 @@ public:
     //    }
 
     // 2.optimized approach
-    sort(nums.begin(),nums.end());
-    int count=1;
-    int ans=nums[0];
-    for(int i=1;i<n;i++){
-        if(nums[i]==nums[i-1]){
-            count++;
-            ans=nums[i];
-        }else{
-            count=1;
-            ans=nums[i];
-        }
-         
-         if(count>n/2){
-            return ans;
-        }
-      
-     
-    }
+    // sort(nums.begin(),nums.end());
+    // int count=1;
+    // int ans=nums[0];
+    // for(int i=1;i<n;i++){
+    //     if(nums[i]==nums[i-1]){
+    //         count++;
+    //         ans=nums[i];
+    //     }else{
+    //         count=1;
+    //         ans=nums[i];
+    //     }      
+    //      if(count>n/2){
+    //         return ans;
+    //     }
+    // }
+    //    return -1;
 
-     
-    
-    
-       return -1;
+    // 3.moore's voting algo
+    int freq=0,ans=0;
+    for(int i=0;i<n;i++){
+        if(freq==0){
+            ans=nums[i];
+        }
+        if(ans==nums[i]){
+            freq++;
+        }else{
+            freq--;
+        }
+    }
+    return ans;
     }
 };
