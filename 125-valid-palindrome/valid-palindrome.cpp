6f987000fc91153b067272ead5a616d528fb27fd
student-chetankumar <chetankumar98672@gmail.com>
2026-07-28 14:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (54.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+bool solve(string &s, int st,int e){
+    while(st<e && !isalnum(s[st])) st++;
+    while(st<e && !isalnum(s[e])) e--;
+    
+    if(st>=e) return true;
+    if(tolower(s[st])!=tolower(s[e])){
+       return false;
+    }
+    return solve(s,st+1,e-1);
+
+}
     bool isPalindrome(string s) {
         
-      int st=0,e=s.size()-1;
-      while(st<e){
-        if(!isalnum(s[st]))  st++ ;
-        else if(!isalnum(s[e]))  e--;
-        else {
-            if(tolower(s[st])!=tolower(s[e])) return false;
-                st++;
-                e--;
-            
-        }
-      }  
-       return true;
+    //   int st=0,e=s.size()-1;
+    //   while(st<e){
+    //     if(!isalnum(s[st]))  st++ ;
+    //     else if(!isalnum(s[e]))  e--;
+    //     else {
+    //         if(tolower(s[st])!=tolower(s[e])) return false;
+    //             st++;
+    //             e--;
+    //     }
+    //   }  
+       return solve(s,0,s.length());
     }
 };
