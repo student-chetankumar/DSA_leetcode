33498699b50833519ed7843bce1553bd99ed0699
student-chetankumar <chetankumar98672@gmail.com>
2026-07-28 14:02:30 +0530

    Time: 15 ms (22.66%) | Memory: 7.6 MB (88.96%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-// int topdown(int n , vector<int>& dp){
-//     if( n==0 || n==1 ) return n;
-//     if(dp[n]!=-1) return dp[n];
-//     dp[n] =  topdown(n-1,dp)+topdown(n-2,dp);
-//     return dp[n];
-// }
-    int fib(int n) {
-        int a=0,b=1;
-        int ans;
-        if(n==0) return 0;
-        if(n==1) return 1;
-        for(int i=2 ; i<=n;i++){
-            ans=a+b;
-            a=b;
-            b=ans;
-        }
+int topdown(int n){
+    if( n==0 || n==1 ) return n;
+    
+    int ans =  topdown(n-1)+topdown(n-2);
     return ans;
+}
+    int fib(int n) {
+    //     int a=0,b=1;
+    //     int ans;
+    //     if(n==0) return 0;
+    //     if(n==1) return 1;
+    //     for(int i=2 ; i<=n;i++){
+    //         ans=a+b;
+    //         a=b;
+    //         b=ans;
+    //     }
+    // return ans;
     // // return fib(n-1)+fib(n-2);
     // vector<int>dp(n+1,-1);
-    // int ans = topdown(n , dp);
-    // return ans;
+    int ans = topdown(n);
+    return ans;
     }
 };
