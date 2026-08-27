b399d7fc5446ff11c19089c964ff0f7c68473f6e
student-chetankumar <chetankumar98672@gmail.com>
2026-08-27 18:46:30 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (67.30%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,14 +11,34 @@
  */
 class Solution {
 public:
+bool solve(TreeNode* root,int targetSum,int sum){
+    if(root==NULL) return false;
+
+    sum = sum+root->val;
+
+    if(root->left==NULL && root->right==NULL ){
+        if(sum==targetSum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    bool lAns = solve(root->left,targetSum,sum);
+    bool rAns = solve(root->right,targetSum,sum);
+    return lAns || rAns;
+}
     bool hasPathSum(TreeNode* root, int targetSum) {
    
-    if(!root) return false;
-    targetSum=targetSum-root->val;
-    if(root->left == NULL && root->right==NULL && targetSum==0){
-       return true;
-    }
-    return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
+    // if(!root) return false;
+    // targetSum=targetSum-root->val;
+    // if(root->left == NULL && root->right==NULL && targetSum==0){
+    //    return true;
+    // }
+    // return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
      
+     int sum =0 ;
+     bool ans = solve(root,targetSum,sum);
+     return ans;
     }
 };
