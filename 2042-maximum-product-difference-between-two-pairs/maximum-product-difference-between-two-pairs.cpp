ef96763aebc12d2a489864b148517f58a2a5e0a2
student-chetankumar <chetankumar98672@gmail.com>
2026-09-22 17:55:29 +0530

    Time: 27 ms (17.42%) | Memory: 24 MB (69.76%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-      int firstmax=0;
-      int secondmax=0;
-      int firstmin=INT_MAX;
-      int secondmin=INT_MAX;
+    //   int firstmax=0;
+    //   int secondmax=0;
+    //   int firstmin=INT_MAX;
+    //   int secondmin=INT_MAX;
 
-        for(auto x:nums){
-            if(x>firstmax){
+    //     for(auto x:nums){
+    //         if(x>firstmax){
                 
-                secondmax=firstmax;
-                firstmax=x;
-            }
-            else if(secondmax<x){
-                secondmax=x;
-            }
-        }
+    //             secondmax=firstmax;
+    //             firstmax=x;
+    //         }
+    //         else if(secondmax<x){
+    //             secondmax=x;
+    //         }
+    //     }
 
-        for(auto x:nums){
-            if(x<firstmin){
+    //     for(auto x:nums){
+    //         if(x<firstmin){
                 
-                secondmin=firstmin;
-                firstmin=x;
-            }
-            else if(secondmin>x){
-                secondmin=x;
-            }
-        }
-        int ans= (firstmax*secondmax)-(firstmin*secondmin);
-        return ans;
+    //             secondmin=firstmin;
+    //             firstmin=x;
+    //         }
+    //         else if(secondmin>x){
+    //             secondmin=x;
+    //         }
+    //     }
+    //     int ans= (firstmax*secondmax)-(firstmin*secondmin);
+    //     return ans;
+
+    // OR using sort function
+    sort(nums.begin(),nums.end());
+    int n=nums.size();
+    return ((nums[n-1]*nums[n-2])-(nums[0]*nums[1]));
     }
 };
